7e1a65445218e48594dffbaa3783638d8c9e5e90
Muratcan Akçay <muratcanakcay@gmail.com>
2021-03-12 12:58:19 +0100

    Update resource.h

--- FlappyBird/FlappyBird/resource.h
+++ FlappyBird/FlappyBird/resource.h
@@ -12,6 +12,10 @@
 #define IDI_SMALL                       108
 #define IDC_FLAPPYBIRD                  109
 #define IDR_MAINFRAME                   128
+#define ID_FILE_H                       32773
+#define ID_H_1                          32774
+#define ID_H_2                          32775
+#define ID_H_3                          32776
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,7 +24,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        129
-#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_COMMAND_VALUE         32777
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
