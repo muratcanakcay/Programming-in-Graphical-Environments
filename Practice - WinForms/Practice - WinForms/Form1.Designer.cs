54da8232121ba7dc18e760e3ba5e50f9c1508574
Muratcan Akçay <muratcanakcay@gmail.com>
2021-03-30 23:57:01 +0200

    Practicing WinForms

---
+++ Practice - WinForms/Practice - WinForms/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Practice___WinForms
+{
+    partial class PracticeWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.nameErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.nameErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(45, 32);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(67, 15);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Your name:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(45, 61);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 23);
+            this.nameTextBox.TabIndex = 1;
+            this.nameTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.nameTextBox_KeyPress);
+            this.nameTextBox.Validating += new System.ComponentModel.CancelEventHandler(this.nameTextBox_Validating);
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(57, 104);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 2;
+            this.goButton.Text = "OK";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // nameErrorProvider
+            // 
+            this.nameErrorProvider.ContainerControl = this;
+            // 
+            // PracticeWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(184, 161);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "PracticeWindow";
+            this.Text = "PracticeWindow";
+            ((System.ComponentModel.ISupportInitialize)(this.nameErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.ErrorProvider nameErrorProvider;
+    }
+}
+
---
+++ Practice - WinForms/Practice - WinForms/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practice___WinForms
+{
+    public partial class PracticeWindow : Form
+    {
+        public PracticeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            if (this.ValidateChildren())
+                MessageBox.Show("Welcome " + nameTextBox.Text);
+        }
+
+        private void nameTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(this.ValidateChildren() && e.KeyChar.Equals((char)Keys.Return))
+                MessageBox.Show("Welcome " + nameTextBox.Text);
+        }
+
+        private void nameTextBox_Validating(object sender, CancelEventArgs e)
+        {
+            if (nameTextBox.Text.Length == 0 || nameTextBox.Text.Length >6)
+            {
+                nameErrorProvider.SetError(nameTextBox, "Name must be 1 to 6 characters");
+                e.Cancel = true;
+                return;
+            }
+
+            nameErrorProvider.SetError(nameTextBox, string.Empty);
+            e.Cancel = false;
+        }
+    }
+}
