02a1e428f777d4ea59caed2ba3af7824b7d58d18
Muratcan Akçay <muratcanakcay@gmail.com>
2021-03-31 18:26:45 +0200

    Lab 05 - LabPart - Submitted Version

---
+++ Lab05 - Winforms - CoverDesigner - LabPart/Lab05/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace Lab05
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.Canvas = new System.Windows.Forms.PictureBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.addTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.addTextLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.authorLabel = new System.Windows.Forms.Label();
+            this.authorTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.splitContainer.Panel1.Controls.Add(this.Canvas);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.splitContainer.Panel2.Controls.Add(this.addButton);
+            this.splitContainer.Panel2.Controls.Add(this.addTextBox);
+            this.splitContainer.Panel2.Controls.Add(this.textBox2);
+            this.splitContainer.Panel2.Controls.Add(this.addTextLabel);
+            this.splitContainer.Panel2.Controls.Add(this.titleLabel);
+            this.splitContainer.Panel2.Controls.Add(this.authorLabel);
+            this.splitContainer.Panel2.Controls.Add(this.authorTextBox);
+            this.splitContainer.Panel2MinSize = 200;
+            this.splitContainer.Size = new System.Drawing.Size(1384, 861);
+            this.splitContainer.SplitterDistance = 1170;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.Color.White;
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(1170, 861);
+            this.Canvas.TabIndex = 0;
+            this.Canvas.TabStop = false;
+            this.Canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.Canvas_Paint);
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseEnter += new System.EventHandler(this.Canvas_MouseEnter);
+            this.Canvas.MouseLeave += new System.EventHandler(this.Canvas_MouseLeave);
+            // 
+            // addButton
+            // 
+            this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addButton.Location = new System.Drawing.Point(3, 378);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(200, 23);
+            this.addButton.TabIndex = 6;
+            this.addButton.Text = "Add Text";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // addTextBox
+            // 
+            this.addTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.addTextBox.Location = new System.Drawing.Point(3, 196);
+            this.addTextBox.MinimumSize = new System.Drawing.Size(200, 4);
+            this.addTextBox.Multiline = true;
+            this.addTextBox.Name = "addTextBox";
+            this.addTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.addTextBox.Size = new System.Drawing.Size(200, 175);
+            this.addTextBox.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(3, 31);
+            this.textBox2.MinimumSize = new System.Drawing.Size(200, 4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 92);
+            this.textBox2.TabIndex = 4;
+            // 
+            // addTextLabel
+            // 
+            this.addTextLabel.AutoSize = true;
+            this.addTextLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.addTextLabel.Location = new System.Drawing.Point(3, 174);
+            this.addTextLabel.Name = "addTextLabel";
+            this.addTextLabel.Size = new System.Drawing.Size(111, 19);
+            this.addTextLabel.TabIndex = 1;
+            this.addTextLabel.Text = "Additional Text";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.titleLabel.Location = new System.Drawing.Point(3, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(38, 19);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Title";
+            // 
+            // authorLabel
+            // 
+            this.authorLabel.AutoSize = true;
+            this.authorLabel.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.authorLabel.Location = new System.Drawing.Point(3, 126);
+            this.authorLabel.Name = "authorLabel";
+            this.authorLabel.Size = new System.Drawing.Size(55, 19);
+            this.authorLabel.TabIndex = 2;
+            this.authorLabel.Text = "Author";
+            // 
+            // authorTextBox
+            // 
+            this.authorTextBox.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.authorTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.authorTextBox.Location = new System.Drawing.Point(3, 148);
+            this.authorTextBox.MinimumSize = new System.Drawing.Size(200, 4);
+            this.authorTextBox.Name = "authorTextBox";
+            this.authorTextBox.Size = new System.Drawing.Size(200, 23);
+            this.authorTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1384, 861);
+            this.Controls.Add(this.splitContainer);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainWindowLoad);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            this.splitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.PictureBox Canvas;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox addTextBox;
+        private System.Windows.Forms.Label addTextLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label authorLabel;
+        private System.Windows.Forms.TextBox authorTextBox;
+        private System.Windows.Forms.Button addButton;
+    }
+}
+
---
+++ Lab05 - Winforms - CoverDesigner - LabPart/Lab05/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Lab05
+{
+    public partial class Form1 : Form
+    {
+        bool addText = false;
+        struct addedText
+        {
+            public string text;
+            public int x;
+            public int y;
+        }
+        List<addedText> addedTexts = new List<addedText>();
+            
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindowLoad(object sender, EventArgs e)
+        {
+            //splitContainer.SplitterDistance = Convert.ToInt32(this.ClientSize.Width * 0.66);
+            //Canvas.Width = splitContainer.Panel1.Width;
+            //Canvas.Height = splitContainer.Panel1.Height;
+
+            
+
+            
+        }
+
+        private void Canvas_Paint(object sender, PaintEventArgs e)
+        {
+            int x = Canvas.Width / 4;
+            int y = Canvas.Height / 4;
+            int margin = 20;
+
+            e.Graphics.DrawRectangle(Pens.DarkGray, x, y, x - margin, 2*y);
+            e.Graphics.DrawRectangle(Pens.DarkGray, 2*x - margin, y, 2*margin, 2*y);
+            e.Graphics.DrawRectangle(Pens.DarkGray, 2*x + margin, y, x - margin, 2*y);
+
+            FontFamily fontFamily = new FontFamily("Arial");
+            Font font = new Font(
+                       fontFamily,
+                       16,
+                       FontStyle.Regular,
+                       GraphicsUnit.Pixel);
+            StringFormat myStringFormat = new StringFormat();
+
+            foreach (var t in addedTexts)
+                e.Graphics.DrawString(t.text, font, Brushes.Black, t.x, t.y, myStringFormat);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            addText = true;
+        }
+
+        private void Canvas_MouseEnter(object sender, EventArgs e)
+        {
+            if (addText)
+                this.Cursor = Cursors.Cross;
+        }
+
+        private void Canvas_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Arrow;
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (addText)
+            {
+                Graphics g = Canvas.CreateGraphics();
+
+                FontFamily fontFamily = new FontFamily("Arial");
+                Font font = new Font(
+                           fontFamily,
+                           16,
+                           FontStyle.Regular,
+                           GraphicsUnit.Pixel);
+                StringFormat myStringFormat = new StringFormat();
+
+                float textWidth = g.MeasureString(addTextBox.Text, font).Width;
+                float textHeight = g.MeasureString(addTextBox.Text, font).Height;
+
+
+                addedTexts.Add( new addedText { text = addTextBox.Text, x = e.X - (int)textWidth/2, y = e.Y - (int)textHeight / 2 } );
+
+                foreach (var t in addedTexts) 
+                    Debug.WriteLine($"{t.text}, {t.x},  {t.y}");
+                
+
+                
+
+                foreach (var t in addedTexts)
+                    g.DrawString(t.text, font, Brushes.Black, t.x, t.y, myStringFormat);
+
+                addTextBox.Text = "";
+                addText = false;
+            }
+        }
+    }
+
+    
+}
