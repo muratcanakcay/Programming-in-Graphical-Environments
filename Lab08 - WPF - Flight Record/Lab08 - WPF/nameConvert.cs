3c9d1eabff6e08bd53ea18d9c884f1151e9a38c5
Muratcan Akcay <muratcanakcay@gmail.com>
2021-05-07 18:47:07 +0200

    Lab08- Flight Record - Submitted version

---
+++ Lab08 - WPF - Flight Record/Lab08 - WPF/nameConvert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Lab08___WPF
+{
+    public class nameConvert : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null)
+            {
+                return values[0] + " " + values[1];
+            }
+
+            return values;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Lab08 - WPF - Flight Record/Lab08 - WPF/timeConvert.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Lab08___WPF
+{
+    public class timeConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var minutes = $"{((TimeSpan)value).Hours*60+((TimeSpan)value).Minutes}";
+                var retVal = minutes + " Minutes";
+                return retVal;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
