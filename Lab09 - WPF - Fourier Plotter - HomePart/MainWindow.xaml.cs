65351199296285925c71dff2ea9a86db4af96824
Muratcan Akcay <muratcanakcay@gmail.com>
2021-05-15 13:02:18 +0200

    Radius change triggers recalculation of tips and centers. WIP: DrawTrail().

--- Lab09 - WPF - Fourier Plotter - HomePart/MainWindow.xaml.cs
+++ Lab09 - WPF - Fourier Plotter - HomePart/MainWindow.xaml.cs
@@ -29,9 +29,10 @@ namespace Lab09___WPF___Fourier_Plotter___HomePart
     {
         private DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Render);
         private int tickCount = 0;
-        private ObservableCollection<UIElement> UIElements = new ObservableCollection<UIElement>();
         private Point CanvasCenter = new Point();
         private CircleList circleList = new CircleList();
+        private ObservableCollection<UIElement> DrawnCircles = new ObservableCollection<UIElement>();
+        private ObservableCollection<UIElement> DrawnLines = new ObservableCollection<UIElement>();
         
        public static MainWindow mw;
 
@@ -46,13 +47,13 @@ namespace Lab09___WPF___Fourier_Plotter___HomePart
             timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
             timer.Tick += new EventHandler(Tick);
 
-            //mw = this;
+            mw = this;
 
             // ----------------
 
             XmlSerializer deserializer = new XmlSerializer(typeof(CircleList));
 
-            TextReader reader = new StreamReader("test.xml");
+            TextReader reader = new StreamReader("test2.xml");
             
             circleList = (CircleList)deserializer.Deserialize(reader);
 
@@ -63,14 +64,7 @@ namespace Lab09___WPF___Fourier_Plotter___HomePart
                 Debug.WriteLine($"{c.radius}, {c.frequency}");
             }
 
-            circleList.circleList[0].StartingCenter = new Point(0, 0);
-            circleList.circleList[0].tip = new Point(circleList.circleList[0].radius, 0);
-
-            for (int i = 1; i < circleList.circleList.Count; i++)
-            {
-                circleList.circleList[i].StartingCenter = circleList.circleList[i - 1].tip;
-                circleList.circleList[i].tip = new Point(circleList.circleList[i].StartingCenter.X + circleList.circleList[i].radius, circleList.circleList[i].StartingCenter.Y);
-            }
+            CalculateTipsAndCenters();
 
             DataContext = circleList.circleList;
 
@@ -83,6 +77,18 @@ namespace Lab09___WPF___Fourier_Plotter___HomePart
 
             // -----------
 
+        }
+
+        private void CalculateTipsAndCenters()
+        {
+            circleList.circleList[0].StartingCenter = new Point(0, 0);
+            circleList.circleList[0].tip = new Point(circleList.circleList[0].radius, 0);
+
+            for (int i = 1; i < circleList.circleList.Count; i++)
+            {
+                circleList.circleList[i].StartingCenter = circleList.circleList[i - 1].tip;
+                circleList.circleList[i].tip = new Point(circleList.circleList[i].StartingCenter.X + circleList.circleList[i].radius, circleList.circleList[i].StartingCenter.Y);
+            }
         }
 
         private void ExitButtonClicked(object sender, RoutedEventArgs e)
@@ -150,7 +156,8 @@ namespace Lab09___WPF___Fourier_Plotter___HomePart
                 {
                     PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
                     
-                    //mw.Tick();
+                    Debug.WriteLine($"Property changed");
+                    mw.CalculateTipsAndCenters();
                 }
             }
 
@@ -195,6 +202,8 @@ namespace Lab09___WPF___Fourier_Plotter___HomePart
 
         public void Tick(object sender, EventArgs e)
         {
+            
+
             if(++tickCount >= 666)
             {
                 timer.Stop();
@@ -202,27 +211,52 @@ namespace Lab09___WPF___Fourier_Plotter___HomePart
             }
             
             ++progressBar.Value;
-            theCanvas.Children.Clear();
-                
-            Circle previousCircle = (Circle)null;
+            
+            foreach(var circle in DrawnCircles)
+                theCanvas.Children.Remove(circle);
+
+            foreach (var line in DrawnLines)
+                theCanvas.Children.Remove(line);
+
+            Circle lastCircle = (Circle)null;
             foreach (Circle c in circleList.circleList)
             {
-                if (previousCircle==null)
+                if (lastCircle==null)
                 {
                     c.Rotate(tickCount);
+                    lastCircle = c;
+                    
                     DrawCircle(c);
                     DrawLine(c);
-                    previousCircle = c;
+                    
                     continue;
                 }
-                c.center = previousCircle.tip;
+                c.center = lastCircle.tip;
                 c.Rotate(tickCount);
-                previousCircle = c;
+                lastCircle = c;
+                
                 DrawCircle(c);
                 DrawLine(c);
             }
+
+            DrawTrail(lastCircle);
         }
 
+        public void DrawTrail(Circle c)
+        {
+                Ellipse trailEllipse = new Ellipse();
+                //myEllipse.Cursor = Cursors.Hand;
+                SolidColorBrush brush = new SolidColorBrush();
+                brush.Color = Color.FromArgb(255, 255, 0, 0);
+                trailEllipse.Stroke = brush;
+                trailEllipse.Width = 3;
+                trailEllipse.Height = 3;
+                
+                Canvas.SetLeft(trailEllipse, CanvasCenter.X + c.tip.X) ;
+                Canvas.SetTop(trailEllipse, CanvasCenter.Y + c.tip.Y) ;
+                theCanvas.Children.Add(trailEllipse);
+        }
+        
         public void DrawCircle(Circle c)
         {
                 Ellipse myEllipse = new Ellipse();
@@ -233,7 +267,7 @@ namespace Lab09___WPF___Fourier_Plotter___HomePart
                 myEllipse.Width = 2 * c.radius;
                 myEllipse.Height = 2 * c.radius;
             
-                UIElements.Add(myEllipse);
+                DrawnCircles.Add(myEllipse);
                 
                 Canvas.SetLeft(myEllipse, CanvasCenter.X + c.center.X - c.radius) ;
                 Canvas.SetTop(myEllipse, CanvasCenter.Y + c.center.Y - c.radius) ;
@@ -251,7 +285,7 @@ namespace Lab09___WPF___Fourier_Plotter___HomePart
                 myLine.X2 = c.tip.X;
                 myLine.Y2 = c.tip.Y;
             
-                UIElements.Add(myLine);
+                DrawnLines.Add(myLine);
                 
                 Canvas.SetLeft(myLine, CanvasCenter.X) ;
                 Canvas.SetTop(myLine, CanvasCenter.Y) ;
